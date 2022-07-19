f4779868867b281cb810f70c03de4d90a606d71b
John Ugbor <ugborjohn@gmail.com>
2022-07-19 11:01:08 +0100

    added redux, redux toolkit and created chat store

---
+++ src/store/slice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { combineReducers } from "redux";
+
+export const chatSlice = createSlice({
+  name: "chat",
+  initialState: {
+    data: [{}],
+  },
+  reducers: {
+    updateChat: (state, action) => ({
+      ...state,
+      data: action.payload,
+    }),
+  },
+});
+
+const reducer = combineReducers({
+  chat: chatSlice.reducer,
+});
+
+export const { updateChat } = chatSlice.actions;
+
+export default reducer;
